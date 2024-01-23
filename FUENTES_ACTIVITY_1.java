d23ffa7a08d2cef334734b3ea19fbbbdf2e8373f
Jheloni Fuentes <156734392+jheloni@users.noreply.github.com>
2024-01-23 11:33:46 +0800

    Add files via upload

---
+++ FUENTES_ACTIVITY_1.java
@@ -0,0 +1,16 @@
+public class FUENTES_ACTIVITY_1{
+    public static void main(String []args){
+
+       int x = 20;
+       int c = 30;
+       int v = 65;
+
+       System.out.println(x - v);
+       System.out.println(x + c);
+       System.out.println(v / x);
+       System.out.println(v * c);
+
+
+    }
+}
+
