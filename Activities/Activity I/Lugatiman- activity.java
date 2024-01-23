430e1bd7147b73d3bbf55a1662fe23f449a51df1
Michelle Grace S. Lugatiman <156732421+lugatiman@users.noreply.github.com>
2024-01-23 11:31:42 +0800

    Add files via upload

---
+++ Activities/Activity I/Lugatiman- activity.java
@@ -0,0 +1,16 @@
+class Lugatiman {
+    public static void main ( String[]args){
+        int x = 60;
+        int y = 50;
+        int f = 110;
+        int b = x + y;
+        int a = y * f;
+        int e = f - y;
+
+        System.out.println("SUM" + b);
+        System.out.println("PRODUCT" + a);
+        System.out.println("DIFFERENCE" + e);
+    }
+    
+}
+
