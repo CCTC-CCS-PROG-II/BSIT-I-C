89704f7835af60d65e8c724878824c8640ae34ce
Jenishe A. Broncano <156733649+JenisheBroncano@users.noreply.github.com>
2024-01-23 12:35:49 +0800

    Add files via upload

---
+++ [BRONCANO_ACTIVITY_1].java
@@ -0,0 +1,16 @@
+public class [BRONCANO_ACTIVITY_1 {
+    public static void main(String[]args){
+        int a = 25;
+        int b = 50;
+        int c = 75;
+
+        int d = a+b;
+        int e = a-c;
+        int f = c*a; 
+
+        System.out.println("Addition:" + d);
+        System.out.println("Subtraction:" + e);
+        System.out.println("Multiplication:" + f);
+    
+}   
+}
