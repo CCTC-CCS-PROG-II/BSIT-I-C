21a04216d7b02be8e30f9f31d1b799011c920bdf
Kathlien grace Arnido <156732552+kathlien@users.noreply.github.com>
2024-01-23 11:12:59 +0800

    Add files via upload

---
+++ Activities/Activity I/Arnido_ACTIVITY_I.java
@@ -0,0 +1,11 @@
+public class Arnido_ACTIVITY_I {
+    public static void main (String [] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+        int d = a + b + c;
+
+        System.out.println(d);
+    }
+}
