20573d198bcff1a5872f49475736502e33564ba2
Tylo Miguel Yungot <156736078+taylolo@users.noreply.github.com>
2024-01-23 11:24:32 +0800

    Create YUNGOT_ACTIVITY_1.java

---
+++ Activities/Activity I/YUNGOT_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+public class YUNGOT_ACTIVITY_I {
+  public static void main (String []args) {
+
+      int Q = 11;
+      int W = 32;
+      int E = 23;
+
+      int A = Q + W;
+      int B = W - Q;
+      int C = Q * E;
+      int D = E / Q;
+
+      System.out.println("Addition = "+A); //Addition
+      System.out.println("Substraction = "+B); //Subtraction
+      System.out.println("Multiplication = "+C); //Multiplication
+      System.out.println("Division = "+D); //Division
+  }
+}
+  
