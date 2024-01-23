19a677e942172b9482f5acb5b425042d9b6390c1
Kent Harjel S. Alibango <145254312+kentharjel@users.noreply.github.com>
2024-01-23 11:25:15 +0800

    Add files via upload

---
+++ Activities/Activity I/alibango_act_1.java
@@ -0,0 +1,19 @@
+class aliabngo_act_1 {
+    public static void main(String[] args) {
+        int X = 10;
+        int Y = 50;
+        int Z = 100;
+
+        int D = X + Y;
+        int E = X * Y;
+        int F = Z - Y;
+        int G = Z / X;
+
+        System.out.println("Addition:" + D);
+        System.out.println("Multiplication:" + E);
+        System.out.println("Subtraction:" + F);
+        System.out.println("Division:" + G);
+
+    }
+    
+}
