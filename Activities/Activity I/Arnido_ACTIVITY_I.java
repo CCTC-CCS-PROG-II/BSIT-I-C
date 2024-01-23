f21c1e0cb1519789c7690180ac9c62dfce63e5d8
Kathlien grace Arnido <156732552+kathlien@users.noreply.github.com>
2024-01-23 11:32:03 +0800

    Update Arnido_ACTIVITY_I.java

--- Activities/Activity I/Arnido_ACTIVITY_I.java
+++ Activities/Activity I/Arnido_ACTIVITY_I.java
@@ -1,11 +1,17 @@
 public class Arnido_ACTIVITY_I {
     public static void main (String [] args) {
-        int a = 5;
-        int b = 10;
-        int c = 15;
+        int a = 2;
+        int b = 4;
+        int c = 6;
 
-        int d = a + b + c;
+        int d = a + b;
+        int e = a* b;
+        int f = b - c;
+        int g = a / b;
 
-        System.out.println(d);
+        System.out.println("Addition:" + d);
+        System.out.println("Multiplication:" + e);
+        System.out.println("Subtraction:" + f);
+        System.out.println("Division:" + g);    
     }
 }
