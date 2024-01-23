a8e509a2409a2568781f31782a4fc2c2cffdc959
Xaviera Marie Alcomendras <156732556+submarine02@users.noreply.github.com>
2024-01-23 11:12:58 +0800

    Add files via upload

---
+++ Activities/Activity I/ALCOMENDRAS_ACTIVITY_1.java
@@ -0,0 +1,10 @@
+public class ALCOMENDRAS_ACTIVITY_1 {
+    public static void main(String[] args) {
+        int h = 20;
+        int i = 25;
+
+        int j = h + i;
+
+        System.out.println(h + i);
+    }
+}
