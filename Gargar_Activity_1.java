0b940ed3cc60125972c5851b3d97191ca7513e96
Jodel Gargar <156733416+Finnnnnny@users.noreply.github.com>
2024-01-23 11:20:49 +0800

    Add files via upload

---
+++ Gargar_Activity_1.java
@@ -0,0 +1,11 @@
+class Gargar_Activity_1 {
+    public static void main(String[] args ) {
+        int x = 3;
+        int y = 3;
+        int z = 3;
+
+        System.out.println(z + z);
+        System.out.println(y * z);
+        System.out.println(x / y);
+    }
+}        
