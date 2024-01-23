67d7080cb9626a756d74ebc67c323659b1b84aba
Jeniña Cabisas <156732411+ninyangg@users.noreply.github.com>
2024-01-23 11:34:51 +0800

    Add files via upload

---
+++ Activities/Activity I/Cabisas_Activity_I.java
@@ -0,0 +1,14 @@
+class Cabisas_Activity_I{
+    public static void main(String[] args){
+        
+        int x = 5;
+        int y = 10;
+        int z = 15;
+
+        System.out.println(x + y);
+        System.out.println(z - y);
+        System.out.println(x * z);
+        System.out.println(y / x);
+    
+    }
+}
