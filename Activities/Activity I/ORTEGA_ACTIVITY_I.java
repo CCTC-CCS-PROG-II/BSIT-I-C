45bb806a4cff6fb5597dbc7996ce530730f6eb84
Jay Martin B. Ortega <145258529+jaymartinortega20@users.noreply.github.com>
2024-01-23 11:15:56 +0800

    Add files via upload

---
+++ Activities/Activity I/ORTEGA_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class ORTEGA_ACTIVITY_I {
+  public static void main (String []args) {
+
+      int Q = 10;
+      int W = 20;
+      int E = 30;
+
+      int A = Q + W;
+      int B = W - Q;
+      int C = Q * E;
+      int D = E / Q;
+
+      System.out.println("Addition = "+A); //Addition
+      System.out.println("Substraction = "+B); //Subtraction
+      System.out.println("Multiplication = "+C); //Multiplication
+      System.out.println("Division = "+D); //Division
+  }
+}
+  
+
