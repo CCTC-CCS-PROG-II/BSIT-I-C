5020414e51170b4a3778c2859815df84b415fb10
Claire Labadlabad <156732347+ClaireLabadlabad@users.noreply.github.com>
2024-01-23 11:12:48 +0800

    Add files via upload

---
+++ Activities/Activity I/LABADLABAD_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class LABADLABAD_ACTIVITY_I {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        int f = 30;
+        int z = x+y;
+        int s = x+f;
+    
+        System.out.println(z);
+        System.out.println(f);
+    
+        
+     
+    }
+}
+
+}
