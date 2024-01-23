adc0a583ebf27e61f415ffd58c07dd5dee5bae39
Hans Rafael U. Villanueva <156732376+loki123456789890@users.noreply.github.com>
2024-01-23 11:30:02 +0800

    Add files via upload

---
+++ Activities/Activity I/VILLANUEVA_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+public class VILLANUEVA_ACTIVITY_I{
+    public static void main(String[] args){
+        
+        int x = 13;
+        int y = 10;
+        int z = 16;
+
+        System.out.println(x + y);
+        System.out.println(x - z);
+        System.out.println(y * x);
+        System.out.println(z / x);
+
+        
+    }
+}
