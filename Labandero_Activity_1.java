847eb6675c326564bf7be277d407cfb82cd4be8c
Carmel Anne Labandero <156732253+caramelanneee@users.noreply.github.com>
2024-01-23 11:17:59 +0800

    Add files via upload

---
+++ Labandero_Activity_1.java
@@ -0,0 +1,12 @@
+class Labandero_Activity_1 {
+    public static void main(String[] args) { 
+        int x = 5;
+        int y = 8;
+        int z = 10;
+        
+        System.out.println(x + y);
+        System.out.println(z * x);
+        System.out.println(z / y);
+        System.out.println(y - x);
+    }
+}
