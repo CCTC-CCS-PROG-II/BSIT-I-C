c5cc37c2b511f42959cf2f8923aee999c1664114
Jodel Gargar <156733416+Finnnnnny@users.noreply.github.com>
2024-01-30 11:13:56 +0800

    Add files via upload

---
+++ Activities/Activity II/GARGAR_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class GARGAR_ACTIVITY_II {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(s:"Enter your first number: ");
+        int num1 = sc.nextInt();
+        System.out.println(s:"Enter your second number: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        int diff = num1 - num2;
+        int prod = num1 * num2;
+
+        System.out.printIn("Sum =" + sum);
+        System.out.printIn("Difference =" + diff);
+        System.out.printIn("Product =" + prod);
+
+
+    }
+
+}    
+
