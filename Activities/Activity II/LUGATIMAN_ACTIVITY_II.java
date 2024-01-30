68e75d554c1d9ca07ad576ab60b9c009e8912b17
Michelle Grace S. Lugatiman <156732421+lugatiman@users.noreply.github.com>
2024-01-30 11:01:21 +0800

    Add files via upload

---
+++ Activities/Activity II/LUGATIMAN_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LUGATIMAN_ACTIVITY_II{
+public static void main (String[]args){
+ Scanner input = new Scanner(System.in);
+System.out.print("Enter first number: ");
+ int fnum = input.nextInt();
+System.out.print("Enter first number: ");
+int snum= input.nextInt();
+
+int diffrencce= fnum-snum;
+int sum= fnum+snum;
+int product= fnum/snum;
+
+System.out.println(" diffrencce= " + diffrencce);
+System.out.println("sum= " + sum);
+System.out.println("product= " + product);
+
+
+
+
+    }
+    
+}
