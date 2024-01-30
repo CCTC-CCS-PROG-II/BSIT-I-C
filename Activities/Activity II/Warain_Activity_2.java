a7ae71173cf441f2c5b588fc550fbb98f4835264
Clarence Nino Warain <156734315+N1N25@users.noreply.github.com>
2024-01-30 11:00:30 +0800

    Add files via upload

---
+++ Activities/Activity II/Warain_Activity_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Warain {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int fnum = input.nextInt();
+        System.out.print("Enter second number: ");
+        int snum = input.nextInt();
+        int sum = fnum + snum;
+        System.out.println("The sum is" +sum);
+        int difference = fnum - snum;
+        System.out.println("The difference" +difference);
+        int product = fnum * snum;
+        System.out.println("The product is" +product);
+    }
+    
+}
