801c40c9833da33d8f4c50ebdeacb6be2d2c7586
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-01-30 11:31:08 +0800

    Add files via upload

---
+++ Activities/Activity II/lastimosa_Activity_II.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class lastimosa_Activity_II {
+    public static void main(String[] args) {
+        Scannner sc = new Scannner(System.in);
+    
+        System.out.print("Enter the first number");
+        int c = scanner.nextInt();
+       
+        System.out.println("Enter the second number");
+        int d = scanner.nextInt();
+        
+        int sum = c + d;
+        int diff = c - d;
+        int product = c * d;
+    
+        System.out.println("Sum = " + sum);
+        System.out.println("Differrence = " + diff);
+        System.out.println("Product = " + product);
+        
+        sc.close();
+
+    }
+}
+
