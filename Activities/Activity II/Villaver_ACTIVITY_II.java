ace897d729a939c72c63049f8bf75c82d643edfd
LhainezyVillaver <156732598+LhainezyVillaver@users.noreply.github.com>
2024-01-30 11:14:00 +0800

    Add files via upload

---
+++ Activities/Activity II/Villaver_ACTIVITY_II.java
@@ -0,0 +1,23 @@
+import java .util.Scanner;
+public class Villaver_ACTIVITY_II{
+    public static void main(Stirng []args){
+        Scanner input= new Scanner (System.in);
+        System.out.print("Enter First number: ");
+        int fnum= input.nextInt();
+        System.out.print(s:"Enter Second number: ");
+        int snum= input.nextInt();
+
+        int add = fnum+snum;
+        int subtract = fnum-snum;
+        int Product = fnum*snum;
+
+
+        System.out.print("sum = "+ add);
+        System.out.print("Difference = "+ subtract);
+        System.out.print("Product = "+ product);
+
+
+
+
+    }
+}
