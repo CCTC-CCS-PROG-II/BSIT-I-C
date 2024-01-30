61bca79bb886b921a5d11fc1efc969858262c7b2
Kenneth Jeminez <59696726+KennethJeminez@users.noreply.github.com>
2024-01-30 10:54:54 +0800

    Update ciro_activity.java

--- Activities/ciro_activity.java
+++ Activities/ciro_activity.java
@@ -6,7 +6,7 @@ class ciro.acitivity.java {
         int y = 30;
 
         System.out.printn(x + z);
-        System.out.printn(x - y);
+        System.out.printn(x - y);jkhsdekvbsdgbkaiyukgbcjigyxfuweqgnyfuwgs
         System.out.printn(y * z);
         System.out.printn(y / x);
 
