45aabcb825c2b0bcd6ac37909074577baed5eeb3
Jan Mitchel Triplitt <153042614+JanMitchel@users.noreply.github.com>
2024-01-30 11:20:15 +0800

    Add files via upload

---
+++ TRIPLITT_ACTIVITIY_I.java
@@ -0,0 +1,18 @@
+public class TRIPLITT_ACTIVITIY_I {
+    public static void main(String[] args){
+        int a = 42;
+        int b = 33;
+        int c = 67;
+
+        int z = a + b;
+        int x = c - b;
+        int p = b * a;
+        int d = b / c;
+
+        System.out.println("Addition: " + z);
+        System.out.println("Subtraction: " + x);
+        System.out.println("Multiplication: " + p);
+        System.out.println("Division: " + d);
+    }
+}
+
