3394c216ced02cbe91b820c3b37137ca1b5a91c8
Jayfford <157442772+Jayfford@users.noreply.github.com>
2024-01-30 10:18:53 +0800

    Add files via upload

---
+++ Activities/Activity I/UGBINADA_ACTIVITY_I.java
@@ -0,0 +1,21 @@
+public class UGBINADA_ACTIVITY_I {
+    public static void main (String[]args){
+        int x = 10;
+        int y = 20;
+        System.out.println(x + y);
+
+        int Q =10;
+        int W =20;
+        int E =30;
+
+        int A = Q + W
+        int B = W - Q
+        int C = Q * E 
+        int D = E / Q
+    
+        System.out.println("Addition ="+A); //Addition
+        System.out.println("Substraction ="+B);//Substraction
+        System.out.println("Multiplication="+C);//Multiplication
+        System.out.println("Division="+D);//Division
+    }
+}
