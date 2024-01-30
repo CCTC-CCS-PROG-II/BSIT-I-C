4d9cff567ed56e5e032f5ef40596e1ce40c9cf7a
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-01-30 11:33:13 +0800

    Add files via upload

---
+++ Activities/Activity II/flores_Activity_2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Flores_Activity_2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int x = input.nextInt();
+        System.out.print("Enter second number: ");
+        int y = input.nextInt();
+       
+        int sum = x + y;
+        int difference = x - y;
+        int product = y * x;
+
+        System.out.println("Sum = "+sum);
+        System.out.println("Difference = "+difference);
+        System.out.println("Product = "+product);
+    }
+}
