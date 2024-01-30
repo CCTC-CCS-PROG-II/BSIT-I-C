8e61849aefc335309b43953e15d7036f3daea83e
Prnce Kyriel T Ciro <156732400+kaykaysmithni@users.noreply.github.com>
2024-01-30 11:17:52 +0800

    Add files via upload

---
+++ Activities/Activity II/ciro_activity_2.java2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class ciro_activity_2.java{
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextlnt();
+
+        System.out.println("Enter second number: ");
+        int b = sc.nextlnt();
+
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b;
+
+        System.out.println("Sum = " + sum);
+        
+        System.out.printlm(Difference = " + diff);
+
+        System.out.println("Product = " + product);
+
+        sc.close();
+    }
+}
+
+
+
+
+        
+
+       
+        int num = scanner.nextLine();
+       
+        int num = scanner.nextLine();
+        int num = num1 + num2;
+
+    }
+}
+
+
+        
+
+
+
+
+
+
+        
+
