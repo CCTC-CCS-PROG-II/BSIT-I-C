e7b04bc6d3d7a4f56ae18215aaad4e57728e78e2
prahinog321 <158120066+prahinog321@users.noreply.github.com>
2024-01-30 10:22:34 +0800

    Add files via upload

---
+++ Activities/Activity I/Prahinog.java
@@ -0,0 +1,19 @@
+ class Prahinog {
+    public static void main(String[] args) {
+        int C = 5;
+        int B = 10;
+        int A = 15;
+
+        int D = A + B;
+        int E = A* B;
+        int F = B - C;
+        int G = A / B;
+
+        System.out.println("Addition:" + D);
+        System.out.println("Multiplication:" + E);
+        System.out.println("Subtraction:" + F);
+        System.out.println("Division:" + G);
+
+    }
+    
+}
