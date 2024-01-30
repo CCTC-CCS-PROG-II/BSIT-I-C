d1f86d1bb5598dc0806d0299bf9e2f51c2aaf4a4
Prnce Kyriel T Ciro <156732400+kaykaysmithni@users.noreply.github.com>
2024-01-30 10:24:12 +0800

    Create ciro_activity.java

---
+++ Activities/ciro_activity.java
@@ -0,0 +1,14 @@
+class ciro.acitivity.java {
+    Public static void main(String []args){
+        
+        int x = 10;
+        int z = 20;
+        int y = 30;
+
+        System.out.printn(x + z);
+        System.out.printn(x - y);
+        System.out.printn(y * z);
+        System.out.printn(y / x);
+
+    }
+}
