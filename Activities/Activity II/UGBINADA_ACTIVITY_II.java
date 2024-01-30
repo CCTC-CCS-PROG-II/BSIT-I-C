13d322fccc71e494ebe7eafa4ba1b68d2dbd979e
Jayfford <157442772+Jayfford@users.noreply.github.com>
2024-01-30 11:03:45 +0800

    Add files via upload

---
+++ Activities/Activity II/UGBINADA_ACTIVITY_II.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class UGBINADA_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your first number: ");
+        int a = sc.nextInt();
+
+        System.out.println("Enter your second number:");
+        int b = sc.nextInt();
+
+       
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b ;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+        System.out.println("Product = " + product);
+   
+        sc.close();
+    }
+}
