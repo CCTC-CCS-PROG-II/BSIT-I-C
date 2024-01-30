b906d87d25aba89b789b99cadc63a177115d79c7
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-01-30 10:09:05 +0800

    Add files via upload

--- Lopez_activity_1.java
+++ Lopez_activity_1.java
@@ -1,15 +1,16 @@
 public class Lopez_activity_1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int a = 25;
         int b = 45;
-        int c = 56
-    
+        int c = 56;
+        int d = 86;
+
         int d = a * b;
         int f = d + c;
         System.out.println(d);
         System.out.println(f);
-        
-    
-     
-      }
+
+    }
+
+}
