7d54058b467c696f26034cba78e97168d000b459
James Karl Heyres <157438154+Jameskarlheyres@users.noreply.github.com>
2024-01-30 11:23:27 +0800

    Add files via upload

---
+++ Activities/Activity I/HEYRES_ACTIVITY-I.java
@@ -0,0 +1,19 @@
+public class ALEDO_ACTIVITY_I {
+    public static void main(String[] args) {
+        int C = 5;
+        int B = 10;
+        int D = 15;
+
+        int D = C + B;
+        int E = C * B;
+        int F = B - C;
+        int G = D / B;
+
+        System.out.println("Addition:" + D);
+        System.out.println("Multiplication:" + E);
+        System.out.println("Subtraction:" + F);
+        System.out.println("Division:" + G);
+
+    }
+    
+}
