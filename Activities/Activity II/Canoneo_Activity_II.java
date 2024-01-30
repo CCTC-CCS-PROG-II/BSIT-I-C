19f3c109b8fc3374626d8977668bc1c0c5335cc9
Cathrine May A. Cañoneo <156732189+CathrineCanoneo@users.noreply.github.com>
2024-01-29 20:12:48 -0700

    Add files via upload

---
+++ Activities/Activity II/Canoneo_Activity_II.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Canoneo_Activity_II{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+
+    System.out.print(  "Enter First Number:");
+    int num1 = sc.nextInt();
+
+    System.out.print(  " Enter Second Number:");
+    int num2 = sc.nextInt();
+
+    int sum = num1 + num2;
+    int diff = num1 - num2;
+    int prod = num1*num2;
+
+    System.out.println("Sum =" + sum);
+    System.out.println("Difference =" + diff);
+    System.out.println("Product =" + prod);
+
+
+  }
+}
