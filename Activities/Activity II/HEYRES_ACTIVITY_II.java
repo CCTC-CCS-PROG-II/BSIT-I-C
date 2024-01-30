f40a1a1c09419cfb2e0e7e57d7b175edc26ca6e5
James Karl Heyres <157438154+Jameskarlheyres@users.noreply.github.com>
2024-01-30 10:58:07 +0800

    Add files via upload

---
+++ Activities/Activity II/HEYRES_ACTIVITY_II.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class HEYRES_ACTIVITY_II.java{
+    public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.print("SUM:");
+
+int a = sc.nextInt();
+int b = sc.nextInt();
+
+int sum = a+b;
+System.out.println(sum);
+
+
+if (sum>=230) {
+    System.out.println("DONE");
+} else {
+    System.out.println("Done");
+}
+
+
+    }
+}
