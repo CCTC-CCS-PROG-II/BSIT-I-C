5ebc13c5c32f032d686f210fac00fcdd868d0ae8
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-02-06 11:19:09 +0800

    Add files via upload

---
+++ Activities/Activity IV/FLORES_ACTIVITY_IV.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+public class FLORES_ACTIVITY_IV{
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+
+        String color;
+        System.out.println("Enter color");
+        color = input.nextLine();
+
+        
+        switch (color){
+             case "red":
+                System.out.println(1);
+                break;
+             case "green":
+                System.out.println(2);
+                break;
+             case "black":
+                System.out.println(3);
+                break;   
+        }
+    }
+}    
