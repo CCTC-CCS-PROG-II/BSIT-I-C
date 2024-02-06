c1dba9244e144ae3f5e73a0debb625212a27bd14
Jay Martin B. Ortega <145258529+jaymartinortega20@users.noreply.github.com>
2024-02-06 10:49:30 +0800

    Add files via upload

---
+++ Activities/Activity IV/ORTEGA_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+      public class ORTEGA_ACTIVITY_IV {
+        public static void main (String []args) {
+          Scanner scanner = new Scanner(System.in);
+
+          String color;
+
+          System.out.println("Enter a color: ");
+          color = scanner.nextLine();
+
+          switch (color) {
+            case "red":
+            System.out.println("1");
+            break;
+            
+            case "green":
+            System.out.println("2");
+            break;
+
+            case "black":
+            System.out.println("3");
+            break;
+            
+          }
+
+          
+      
+        }
+      }  
