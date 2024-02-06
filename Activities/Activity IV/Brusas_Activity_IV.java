cf9dfc84a68b0ba2bd5cd780a51643687c85a3a2
Andrew B. Brusas <156732631+drewbubuli@users.noreply.github.com>
2024-02-06 11:11:24 +0800

    Add files via upload

---
+++ Activities/Activity IV/Brusas_Activity_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Brusas_Activity_II_Activity_IV {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        String color;
+        System.out.print("Enter color");
+        color = input.nextLine();
+
+        switch (color) {
+            case "Red":
+                System.out.println("1");
+                
+                break;
+            case "Green":
+                System.out.println("2");
+                
+                break;
+            case "Black":
+                System.out.println("3");
+                
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter Red, Green, or Black.");
+                break;
+     
+            }
+    }
+
+}
