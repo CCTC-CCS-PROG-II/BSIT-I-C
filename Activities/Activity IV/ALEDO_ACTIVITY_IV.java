90d297eda240b90478c4e0a165a8423dc37e2392
Jay Aledo <156733389+POXI101@users.noreply.github.com>
2024-02-06 10:50:40 +0800

    Add files via upload

---
+++ Activities/Activity IV/ALEDO_ACTIVITY_IV.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ALEDO_ACTIVITY_IV {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String color;
+
+        System.out.println("Enter your color you want to display (Red, Green, Black)");
+        color = scanner.nextLine();
+
+        switch (color) {
+            case "Red":
+                System.out.println("1");
+                
+                break;
+            case "Green":
+                System.out.println("2");
+                
+                break;
+            case "Black":
+                System.out.println("3");
+                
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter Red, Green, or Black.");
+                break;
+        }
+
+       
+    }
+}
