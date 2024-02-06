79056dd9c75aa228c88533fa73765c4f8fd60181
LhainezyVillaver <156732598+LhainezyVillaver@users.noreply.github.com>
2024-02-06 11:19:07 +0800

    Add files via upload

---
+++ Activities/Activity IV/VILLAVER_ACTIVITY-IV.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Warain_Aactivty_4{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a color: ");
+        
+        String color = sc.nextline();
+
+        switch (color){
+            case "red":
+                System.out.println(1);
+                break;
+            case "green":
+                System.out.println(2);
+                break;
+            case "black":
+                System.out.println(3);
+                break;
+
+        } 
+    }
+
+}
