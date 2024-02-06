92315fee5b5f7280d809ca538e40f6a41e41ab3f
Jodel Gargar <156733416+Finnnnnny@users.noreply.github.com>
2024-02-06 11:16:10 +0800

    Add files via upload

---
+++ Activities/Activity IV/GARGAR_ACTIVITY_IV.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GARGAR_ACTIVITY_IV { 
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a color (1 for red, 2 for green, 3 for black): ");
+        String colorCode = sc.nextLine();
+
+        switch (colorCode) {
+    case "Red":
+        System.out.println("1");
+        break;
+    case "Green":
+        System.out.println("2");
+        break;
+    case "Black":
+        System.out.println("3");
+        break;
+       }
+    }
+}       
+
