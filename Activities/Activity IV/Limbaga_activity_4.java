4d9267d53103393bf1dff8d41e1b0c87fc62f32f
Ken M Limbaga <156732339+kenlimbaga@users.noreply.github.com>
2024-02-06 11:03:28 +0800

    Add files via upload

---
+++ Activities/Activity IV/Limbaga_activity_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Limbaga_activity_4 {
+    public static void main (String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the color: ");
+
+        String color = sc.nextLine();
+
+        switch (color) {
+            case "blue":
+                 System.out.println("1");
+                 break;
+            case "black":
+                 System.out.println("2");   
+                break;
+            case "purple":
+                 System.out.println("3");
+               break;
+        }
+        
+          
+        }
+    }
