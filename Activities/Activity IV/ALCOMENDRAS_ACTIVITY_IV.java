e6e673b54e27c2b0edd7d0b7396d816588d7ce0e
Xaviera Marie Alcomendras <156732556+submarine02@users.noreply.github.com>
2024-02-06 11:04:30 +0800

    Add files via upload

---
+++ Activities/Activity IV/ALCOMENDRAS_ACTIVITY_IV.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ALCOMENDRAS_ACTIVITY_IV {
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String color;
+
+        System.out.println("Enter the color of the item:");
+         color = sc.nextLine();
+        
+        switch (color) {
+            case "red":
+                System.out.println(1);
+
+                break;
+            case "green":
+                System.out.println(2);
+
+                break;
+            case "black":
+                System.out.println(3);
+            break;
+
+        }
+
+        
+
+    }
+}
+        
