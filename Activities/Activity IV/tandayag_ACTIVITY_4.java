52ade523599cfdc4b4cc4882e67c641967c543e5
KRIS GERALD TANDAYAG <156736424+yabagskie@users.noreply.github.com>
2024-02-06 10:56:43 +0800

    Add files via upload

---
+++ Activities/Activity IV/tandayag_ACTIVITY_4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class tandayag_ACTIVIY_4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number only 1-3: ");
+
+       String color = sc.nextLine();
+
+switch (color){
+       case "red":
+       System.out.println("1");
+       break;
+
+       case "green":
+       System.out.println("2");
+       break;
+
+       case "black":
+       System.out.println("3 ");
+       }
+    }
+}
+
+
+
+
