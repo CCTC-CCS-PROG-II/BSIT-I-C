c1edae0162043ba184a9278154f59bfc662c36ef
Jenishe A. Broncano <156733649+JenisheBroncano@users.noreply.github.com>
2024-02-06 11:02:11 +0800

    Add files via upload

---
+++ Activities/Activity IV/Broncano_Activity_IV.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Broncano_Activity_IV{
+    public static void main(String[] args){
+        Scanner sc = new Scanner ( System.in);
+
+    System.out.print( "Enter the color of the item:");
+    String color = sc.nextLine();
+
+    switch (color){
+        case "red":
+            System.out.println(1);
+            break;
+        case "green":
+            System.out.println(2);
+            break;
+        case "black":
+            System.out.println(3);
+            break;
+            default:
+            break;
+            
+            }
+    }
+}
