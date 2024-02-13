addd4ef0b8e8c3a1e466954f9048a43083280fd6
Claire Labadlabad <156732347+ClaireLabadlabad@users.noreply.github.com>
2024-02-13 10:34:17 +0800

    Add files via upload

---
+++ Activities/Activity VI/LABDLABAD_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class LABDLABAD_ACTIVITY_VI{
+    public static void main (String[]args){
+
+    Scanner sc= new Scanner(System.in);
+    
+    System.out.print("Enter the no. of seconds: ");
+    int x=0;
+    
+    while{
+        System.out.println(" Countdown");
+        x++;
+    }
+    }
+    }
