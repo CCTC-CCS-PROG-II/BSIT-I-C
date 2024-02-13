0521b8d8e3faa859593e37f1747d1ec4b1d3d02d
Tylo Miguel Yungot <158119781+taylol0@users.noreply.github.com>
2024-02-13 10:31:21 +0800

    Add files via upload

---
+++ Activities/Activity VI/YUNGOT_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class YUNGOT_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int num;
+        System.out.println("Number of seconds");
+        int count = sc.nextInt();
+
+        while (count >=0){
+            System.out.println("count");
+            count --;
+        }
+
+    }
+}
+    
