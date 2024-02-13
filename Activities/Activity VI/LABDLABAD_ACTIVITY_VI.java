2f0fe19078efe7e7390efe7bd318dafde00df087
Claire Labadlabad <156732347+ClaireLabadlabad@users.noreply.github.com>
2024-02-13 10:42:12 +0800

    Update LABDLABAD_ACTIVITY_VI.java

--- Activities/Activity VI/LABDLABAD_ACTIVITY_VI.java
+++ Activities/Activity VI/LABDLABAD_ACTIVITY_VI.java
@@ -3,13 +3,13 @@ public class LABDLABAD_ACTIVITY_VI{
     public static void main (String[]args){
 
     Scanner sc= new Scanner(System.in);
-    
+    int num;
     System.out.print("Enter the no. of seconds: ");
-    int x=0;
+    int count = sc.nextInt();
     
-    while{
+    while(count>=100){
         System.out.println(" Countdown");
-        x++;
+        x--;
     }
     }
     }
