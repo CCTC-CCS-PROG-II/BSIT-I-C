46d4a94f329374d4617a4896408e1ed4d1fc8914
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-02-13 10:36:07 +0800

    Add files via upload

---
+++ Activities/Activity VI/LASTIMOSA_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class LASTIMOSA_ACTIVITY_VI {
+    public static void main(Strring[] args) {
+        Scanner sc = new Scanner(Syste.In);
+        System.out.println(Enter the number of seconds: ){
+            int seconds = scannner.nextInt();
+            scanner.close();
+            countdownTimer(seconds);
+    
+        }
+public sstatic void countdownTimer(int seconds){
+    for (int 1 = seconds; i > 0: i--) {
+        Syste.out.println(i);
+    }
+}
+
+}
+}
