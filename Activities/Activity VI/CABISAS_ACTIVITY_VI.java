74990704566d438e5d204bd249456701150a8c2c
Jeniña Cabisas <156732411+ninyangg@users.noreply.github.com>
2024-02-13 10:30:27 +0800

    Add files via upload

---
+++ Activities/Activity VI/CABISAS_ACTIVITY_VI.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CABISAS_ACTIVITY_VI {
+    public static void main(String  []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int seconds = sc.nextInt();
+
+        while(seconds >= 0){
+            System.out.println(seconds);
+            seconds--;
+        }
+    }    
+}
