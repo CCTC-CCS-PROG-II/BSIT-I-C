bc54c927a1dacf76151b3b24fe52b1412b974ca8
Seras Reymann <157442457+reymannseras11@users.noreply.github.com>
2024-02-13 10:32:26 +0800

    Add files via upload

---
+++ Activities/Activity VI/SERAS_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+  public class SERAS_ACTIVITY_VI{
+    public class void main(String[] args);
+       Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number of second ");
+    int second = scanner.nextInt();
+
+    System.out.println("Countdown");
+    while(seconds >= 0) {
+        System.out.println("second;")
+        seconds -=1;
+        
+    
+    }
+  }
+}
