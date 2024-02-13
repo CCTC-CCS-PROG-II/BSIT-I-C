4e1a9c9b9e60c2298fcc984b6a528c8732e40da8
Janine S. Paquit <156732494+janineee321@users.noreply.github.com>
2024-02-13 10:36:08 +0800

    Update and rename PAQUIT_ACTIVITY_XI.java to PAQUIT_ACTIVITY_VI.java

--- Activities/Activity VI/PAQUIT_ACTIVITY_VI.java
+++ Activities/Activity VI/PAQUIT_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PAQUIT_ACTIVITY_XI {
+public class PAQUIT_ACTIVITY_VI {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
