b894924dc8fdca62c174f412a67aae03a2a2bc24
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-02-13 10:33:05 +0800

    Add files via upload

---
+++ Activities/Activity VI/ARANDUQUE_ACTIVITY_IV.java
@@ -0,0 +1,25 @@
+import.java.util.Scanner;
+public class ARANDUQUE_ACTIVITY_IV{
+
+   public static void main(String{} args){
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println(" Enter the colr of the item: ");
+      String color = sc.nextline();
+
+switch (color) {
+    case "red":
+    System.out.println("1");
+    break;
+
+    case "green":
+    System.out.println("2");
+    break;
+
+    case "black"|:
+    System.out.println("3");
+
+     }
+    
+    }
+}
---
+++ Activities/Activity VI/ARANDUQUE_ACTIVITY_VI.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ARANDUQUE_ACTIVITY_VI {
+
+   public static void main(String[] args){
+     Scanner sc = new Scanner (System.in);
+     int num;
+     System.out.println("Enter number: ");
+     int count = sc.nextln();
+
+     while (count >=0){
+        System.out.println(count);
+        cout--;
+
+     }
+
+   }  
+   
+}
