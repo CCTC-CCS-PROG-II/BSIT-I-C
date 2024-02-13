fa5deef9917ae8b6f62a7a61e5ac4793889d51e3
Kent Harjel S. Alibango <145254312+kentharjel@users.noreply.github.com>
2024-02-13 10:34:08 +0800

    Update ALIBANG0_ACTIVITY_VI.java

--- Activities/Activity VI/ALIBANG0_ACTIVITY_VI.java
+++ Activities/Activity VI/ALIBANG0_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class shortcut {
+public class ALIBANGO_ACTIVITY_VI{
     public static void main(String[ ] args) {
 		System.out.print("please input seconds: ");
 		Scanner sc = new Scanner(System.in);
