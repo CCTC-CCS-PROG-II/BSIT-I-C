a860af8ded76197e6b65aada4bf7608e012845b8
Michelle Grace S. Lugatiman <156732421+lugatiman@users.noreply.github.com>
2024-02-13 10:35:24 +0800

    Add files via upload

---
+++ Activities/Activity VI/LUGATIMAN_ACTIVITY_VI.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LUGATIMAN_ACTIVITY_VI {
+    public static void main (String []args){
+
+     Scanner sc= new Scanner(System.in);
+
+    System.out.print("Enter the number of Seconds: ");
+    int input= sc.nextInt();
+
+     
+    int count=5 
+   
+     
+while;
+        do{
+            system.out.println("count:");
+        (x++);
+     }
+
+
+
+     }
+
+}
+    
+
