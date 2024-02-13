82ec0698518887ef9f2d239571bf55220b4487b6
Jay Martin B. Ortega <145258529+jaymartinortega20@users.noreply.github.com>
2024-02-13 10:30:03 +0800

    Add files via upload

---
+++ Activities/Activity VI/ORTEGA_ACTIVITY_VI.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+    public class ORTEGA_ACTIVITY_VI {
+      public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+          int countdown;
+
+          System.out.println("Input Seconds: ");
+          countdown = sc.nextInt();
+
+          while(countdown>0) {
+            System.out.println(countdown);
+            countdown -= 1;
+          }
+
+      }
+    }
