5cea2565ea6c3f513024b39513e4cd4ec568e31d
Ken M Limbaga <156732339+kenlimbaga@users.noreply.github.com>
2024-02-13 10:41:39 +0800

    Add files via upload

---
+++ Activities/Activity VI/Limbaga_Activity_6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+   public class Limbaga_Activity_6{
+    public static void main (String[] args){
+
+    java.util.Scanner sc= new Scanner(System.in);
+    int input = sc.nextInt();
+
+    System.out.println("Enter the number of seconds:");
+
+    int count = 5;
+
+    do{
+        System.out.println("countdown");
+        count ++;
+      } while(count <= 5);
+      
+
+
+    }
+}
