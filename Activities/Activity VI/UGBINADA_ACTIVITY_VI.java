e4b77ec608c198a7336aad4184769b79d51de73b
Jayfford <157442772+Jayfford@users.noreply.github.com>
2024-02-13 10:30:58 +0800

    Add files via upload

---
+++ Activities/Activity VI/UGBINADA_ACTIVITY_VI.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    public class UGBINADA_ACTIVITY_VI{
+
+        public static void main (String[]args){
+            Scanner sc = new Scanner (System.in);
+            System.out.println("Enter the number of seconds");
+            int secs = sc.nextInt();
+
+            while
+            (secs > 0) {
+                System.out.println(secs);
+                secs -=1;
+
+            }
+
+        }
+    }
