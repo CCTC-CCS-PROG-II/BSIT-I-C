650680ed9ba12c167b4b2cdcb14764875ba6d314
LhainezyVillaver <156732598+LhainezyVillaver@users.noreply.github.com>
2024-02-13 10:36:40 +0800

    Add files via upload

---
+++ Activities/Activity VI/VILLAVER_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Villaver_ACTIVITY_VI{
+public Static void main(String[] args);
+
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter the no. of Seconds");
+
+int x=o;
+
+while(x<=100){
+    System.out.prinln("Countdown");
+    x++;
+
+    
+
+
+}
+}
