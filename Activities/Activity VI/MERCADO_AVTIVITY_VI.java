5c75749d2bcb4cebcf7c4884a75741cca4310972
Lianne Gabrielle U. Mercado <156732481+liannemercado@users.noreply.github.com>
2024-02-13 10:30:27 +0800

    Add files via upload

---
+++ Activities/Activity VI/MERCADO_AVTIVITY_VI.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class MERCADO_AVTIVITY_VI {
+   public static void main(String[] args) {
+        Scanner  sc = new Scanner (System.in);
+
+        System.out.println("Enter a number of Seconds:");
+
+       int timer = sc.nextInt();
+
+       while (timer >= 0){
+        System.out.println(timer);
+        timer--;
+
+       }
+
+
+
+
+    }
+}
