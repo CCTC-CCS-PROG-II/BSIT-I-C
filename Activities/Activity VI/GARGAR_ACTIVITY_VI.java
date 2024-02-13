43e37def0dcbf8e29f28aed6076beae57b806e26
Jodel Gargar <156733416+Finnnnnny@users.noreply.github.com>
2024-02-13 10:31:02 +0800

    Add files via upload

---
+++ Activities/Activity VI/GARGAR_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GARGAR_ACTIVITY_VI{
+    public class void main(String[] args);
+       Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number of second");
+    int second = scanner.nextInt(;
+    
+    System.out.println("Countdown");
+    while(seconds > = 0)) {
+        System.out.println(x:"second");
+        seconds -=1;
+    }
+  }
+}      
