09d2cc8a784fc60995a2ad0dfa0e6b0b20ea681a
Jenishe A. Broncano <156733649+JenisheBroncano@users.noreply.github.com>
2024-02-13 10:34:45 +0800

    Add files via upload

---
+++ Activities/Activity VI/Broncano_Activity_VI.java
@@ -0,0 +1,13 @@
+public class Broncano_Activity_VI {
+public static void main(String[] args){
+    Scanner sc = new Scanner ( System.in);
+    int num;
+    System.out.print("Enter number:");
+    int count = sc.nextInt();
+
+    while (count >=0){
+        system.out.print(count);
+        count--;
+    } 
+}     
+}
