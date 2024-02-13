7ecd0e08cdc28f60526f028f3b9f154ca649544d
Jazint <158121033+Jazint@users.noreply.github.com>
2024-02-13 10:35:28 +0800

    Add files via upload

---
+++ Activities/Activity VI/SEGARRA_ACTIVITY_VI.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class SEGARRA_ACTIVITY_VI{
+    public class void main(string[]args);
+
+
+    system.out.printIn("Enter the number of second");
+    int second = scanner nextint(;
+
+    system.out.print("countdown");
+    while(second > = 0){
+
+
+
