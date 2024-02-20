b24e2a7d7b0f0c5d8de32b094de7eb2bc941cb45
Jodel Gargar <156733416+Finnnnnny@users.noreply.github.com>
2024-02-20 10:41:37 +0800

    Add files via upload

---
+++ Activities/Activity VII/Gargar-Activity_VII.java
@@ -0,0 +1,17 @@
+import java.until.Scanner;
+
+public class GARGAR_ACTIVITY_VII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(x:"Enter a number: ");
+        int num = sc.nextInt();
+        long factorial = 1; 
+        for(int i = 1; i <=number; i++) {
+            factorial *= 1;
+            System.out.println(fact);
+            sc.close();
+
+        }
+    }
+}        
