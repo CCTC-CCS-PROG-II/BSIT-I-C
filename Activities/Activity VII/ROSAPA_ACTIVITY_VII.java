e0c3c093a590534b0dd996e852d245d3fa5c0d77
Johniño R. Rosapa <156732181+Johnino123@users.noreply.github.com>
2024-02-20 10:38:23 +0800

    Add files via upload

---
+++ Activities/Activity VII/ROSAPA_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ROSAPA_ACTIVITY_VII {
+    public static void main (String[]args){
+        Scanner sc = new Scanner (System.in);
+
+        long factorial = 1;
+
+        System.out.println("Enter a Non-Negative Integer:");
+        int num = sc.nextInt();
+
+        for(int i=1; i<=num; i++) {
+            factorial*= i;
+        }
+        System.out.println("The Factorial of "+num+" is: "+factorial);
+    }
+}
