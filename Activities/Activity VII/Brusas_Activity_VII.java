542395e305aa0162b1899999c9c818bfc155840e
Andrew B. Brusas <156732631+drewbubuli@users.noreply.github.com>
2024-02-19 18:40:45 -0800

    Add files via upload

---
+++ Activities/Activity VII/Brusas_Activity_VII.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Brusas_Activity_VII {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Factorial Number:");
+
+       int num =sc.nextInt();
+       int fact = 1;
+       for (int x=1;x<=number;x++){
+            fact*=x;
+        }
+        System.out.print(fact);
+        sc.close();
+    }
+}
+    
+
+
+
+
+                                                             
