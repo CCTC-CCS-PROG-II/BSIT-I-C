a810dba2624267a05796450caf8b8b2a9b87336e
Jheloni Fuentes <156734392+jheloni@users.noreply.github.com>
2024-02-20 10:40:15 +0800

    Add files via upload

---
+++ Activities/Activity VII/FUENTES_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class FUENTES_ACTIVITY_VII {
+    public static void main(String []args){
+        Scanner Input = new Scanner(system.in);
+      
+        System.out.print(x:"Enter a number");
+        int = input.nextInt();
+
+        long fatorial = 1;
+        for (int i ) 
+    
+
+
+    
+
+
