0a66e3a2a636fe1676b2aa9c69c56a913e9a8551
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-02-20 10:43:22 +0800

    Add files via upload

---
+++ Activities/Activity VII/LASTIMOSA_ACTIVITY_VII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LASTIMOSA_ACTIVITY_VII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        long factorial = 1;
+       for (int i = 1; i <= number; i++) {
+        factorial *= i;
+        System.out.println("The factorial of" + factorial);
+        sc.close();
+
+       }
+    }
+}
