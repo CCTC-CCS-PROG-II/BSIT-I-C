cf7bfcecb0203a9d10401377eff291d282091f5a
Hans Rafael U. Villanueva <156732376+loki123456789890@users.noreply.github.com>
2024-02-20 10:38:56 +0800

    Add files via upload

---
+++ Activities/Activity VII/VILLANUEVA_ACTIVITY_VII.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class VILLANUEVA_ACTIVITY_VII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        System.out.print("Enter a non negative number: ");
+        int number = sc.nextInt();
+
+        long factorial = 1;
+
+        for(int i = 1; 1 <= num; i++) {
+            factorial *= 1;
+            
+                System.out.println("Factorial of " + number " is: + factorial);
+
+                sc.close();
+            }
+        }
+    }
+
