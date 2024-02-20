f9c9b5928260afca65d8631d993b7b13e70edd03
LhainezyVillaver <156732598+LhainezyVillaver@users.noreply.github.com>
2024-02-20 10:38:55 +0800

    Add files via upload

---
+++ Activities/Activity VII/VILLAVER_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class VILLAVER_ACTIVITY_VII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a non-negative integer: ");
+        int num = sc.nextInt();
+        long factorial = 1;
+
+        for(long i = 1; i <= num; i++){
+            factorial *=i;
+        }
+        System.out.println("The Factorial of " + num + " is " + factorial);
+
+    } 
+}
