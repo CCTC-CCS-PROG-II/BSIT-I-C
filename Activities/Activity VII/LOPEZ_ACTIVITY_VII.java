30a3cdb16981b2dea73be2e952ed0757295e840c
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-02-20 10:39:58 +0800

    Add files via upload

---
+++ Activities/Activity VII/LOPEZ_ACTIVITY_VII.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LOPEZ_ACTIVITY_VII {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter factorial Number: ");
+        long num = sc.nextInt();
+        for (long i = 1; i <= num; i++) {
+            factorial *= 1;
+            System.out.println(factorial);
+
+        }
+    }
+}
