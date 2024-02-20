b2d026377e8352fa58fafe69b7a9015bdfa1a7a4
Janine S. Paquit <156732494+janineee321@users.noreply.github.com>
2024-02-20 10:40:31 +0800

    Add files via upload

---
+++ Activities/Activity VII/PAQUIT_ACTIVITY_VII.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PAQUIT_ACTIVITY_VII {
+    public static main (String[] args){
+        Scanner sc= new Scanner(system.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        long factorial = 1;
+        for (int i =1 ; i <= 1; i++){
+            factorial *= i;
+        } 
+        System.out.println("The factorial of" + num + " is " + factorial);
+    }
+} 
