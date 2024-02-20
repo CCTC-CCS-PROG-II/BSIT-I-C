267b14d2f7c479261ea0b1d2accbcbdf124f1626
prahinog321 <158120066+prahinog321@users.noreply.github.com>
2024-02-20 10:42:12 +0800

    Add files via upload

---
+++ Activities/Activity VII/PRAHINOG_AVTIVITY_VII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+    public class PRAHINOG_5
+    ACTIVITY_VII{
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+            long factorial = 1;
+
+            System.out.print("Enter factorial number: ");
+            long num = sc.nextInt();
+            for(long i=1; i <=num; i++){
+                factorial *=i;
+                System.out.println(factorial);
+            }
+
+        }
+    }
