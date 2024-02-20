b3e849537ffa056975fc72f6bc5796cc1dcb32ef
Clarence Nino Warain <156734315+N1N25@users.noreply.github.com>
2024-02-20 10:38:25 +0800

    Add files via upload

---
+++ Activities/Activity VII/Warain_Activity_VII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Warain_Activity_VII {
+
+    public Static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        long factorial = 1;
+
+        for(int i=1; i <=num; i++) {
+            factorial *= i;
+        }
+        System.out.println("The factorial of " + num + " is " + factorial);
+    }
+}
