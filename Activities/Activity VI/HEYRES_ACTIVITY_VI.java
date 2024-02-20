946859c6ba8c1e2d4d5ae7f7d11a2eee108e47af
James Karl Heyres <157438154+Jameskarlheyres@users.noreply.github.com>
2024-02-20 10:40:39 +0800

    Add files via upload

---
+++ Activities/Activity VI/HEYRES_ACTIVITY_VI.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HEYRES_ACTIVITY_VI{
+    public static void main (String[]args){
+
+    Scanner sc= new Scanner(System.in);
+    int num;
+    System.out.print("Enter the no. of seconds: ");
+    int count = sc.nextInt();
+    
+    while(count>=100){
+        System.out.println(" Countdown");
+        x--;
+    }
+    }
+    }
