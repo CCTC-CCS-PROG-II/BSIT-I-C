d47d30fe7f3e0d94000251204f875fda482a4def
Xaviera Marie Alcomendras <156732556+submarine02@users.noreply.github.com>
2024-02-20 10:38:39 +0800

    Add files via upload

---
+++ Activities/Activity VII/ALCOMENDRAS_ACTIVITY_VII.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class ALCOMENDRAS_ACTIVITY_VII {
+      
+      public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printIn("Enter a number: ");
+        int num = sc.nextInt();
+
+         long factorial = 1;
+
+         for (int i = 1; i <= num; i++) {
+              factorial *= i;
+         } 
+         System.out.println("The factorial of " + num + " is "
+         + factorial);
+    }
+}
+         
+
+
