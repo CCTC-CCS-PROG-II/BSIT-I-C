c23bb1c480e5777f80e3a1009f3c0525889f69cd
Kathlien grace Arnido <156732552+kathlien@users.noreply.github.com>
2024-03-05 11:14:45 +0800

    Add files via upload

---
+++ Activities/Activity VI/ARNIDO_ACTIVITY_VI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ARNIDO_ACTIVITY_VI {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+          int countdown;
+
+          System.out.println("Input Seconds: ");
+          countdown = sc.nextInt();
+
+          while(countdown>0) {
+            System.out.println(countdown);
+            countdown -= 1;
+          }
+
+      }
+    }
+
