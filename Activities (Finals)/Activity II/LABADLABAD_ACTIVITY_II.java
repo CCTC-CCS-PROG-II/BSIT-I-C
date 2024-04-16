6e76ebac45c8b16a1a893b2dbea434c084b393e7
Claire Labadlabad <156732347+ClaireLabadlabad@users.noreply.github.com>
2024-04-16 10:59:43 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/LABADLABAD_ACTIVITY_II.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class LABADLABAD_ACTIVITY_II{
+    public static void main(String[] args){
+            System.out.print("Enter feet/foot: ");
+            Scanner sc = new Scanner(System.in);
+            double feet= sc.nextDouble();
+            
+            convert(num);
+        }
+            static void convert(double num){
+            System.out.println(feet * 12);
+        }
+    }
