00795f66533dd1e805c53058271b3b388584ea52
Jay Martin B. Ortega <145258529+jaymartinortega20@users.noreply.github.com>
2024-04-16 10:19:57 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity I/ORTEGA_ACTIVITY_1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+  public class ORTEGA_ACTIVITY_1 {
+    public static void main (String []args){
+      int length = getLength();
+      int width = getWidth();
+      int area = length * width;
+      System.out.println("Area = "+area);
+    }
+    static int getLength(){
+      Scanner sc = new Scanner(System.int);
+      System.out.println("Enter the length of the rectangle: ");
+      return sc.nextInt();
+    }
+    static int getWidth() {
+      Scanner sc = new Scanner(System.int);
+      System.out.println("Enter the width of the rectangle: ");
+      return sc.nextInt();
+    }
+}
