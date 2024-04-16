47a608a7730db9390e36d9715e48019feabbf45a
Hans Rafael U. Villanueva <156732376+loki123456789890@users.noreply.github.com>
2024-04-16 11:00:52 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/VILLANUEVA_ACTIVITY_II.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class VILLANUEVA_ACTIVITY_II {
+    static void feetToInches(double feet) {
+        double res = feet * 12;
+        System.out.print(res);
+    }
+    public static void main (String[]args){
+        int feet = getFree();
+        int inches = feet * 12;
+        System.out.print(inches);
+    }
+}
