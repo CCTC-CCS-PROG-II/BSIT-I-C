9968811d9304e649118d6862440d4da4704a3de9
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-04-16 10:59:52 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/FLORES_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FLORES_ACTIVITY_II {
+    
+    public static void main(String[] args){
+        System.out.println("Enter feet/foot:");
+        Scanner sc = new Scanner(System.in);
+        double num = sc.nextDouble();
+
+        convert(num);      
+
+    }
+    static void convert(double num){
+        System.out.println(num * 12);
+    }
+}
