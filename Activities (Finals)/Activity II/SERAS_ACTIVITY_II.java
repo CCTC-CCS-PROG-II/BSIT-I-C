c2c8b0ea5b7e4c7870fba5f5a9c2f3b36dbfb988
Seras Reymann <157442457+reymannseras11@users.noreply.github.com>
2024-04-16 11:00:05 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/SERAS_ACTIVITY_II.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SERAS_ACTIVITY_II {
+    static void convert(int feet){
+        int res = feet*12;
+        System.out.println("Inches: "+res);
+    }
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the feet: ");
+        int feet = input.nextInt();
+        convert(feet);
+    }
+    
+}
