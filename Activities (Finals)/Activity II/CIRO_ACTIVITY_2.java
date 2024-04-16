73965c4ed91fa616f9d4018efad282bcedc0ffa3
Prnce Kyriel T Ciro <156732400+kaykaysmithni@users.noreply.github.com>
2024-04-16 10:59:16 +0800

    Add files via upload

---
+++ Activities (Finals)/Activity II/CIRO_ACTIVITY_2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class CIRO_ACTIVITY_2 {
+    static void feetToInches(double feet) {
+        double res = feet * 12;
+        System.out.print(res);
+
+    }
+    public static void main (String[]args) {
+        int feet = getFree();
+        int inches = feet * 12;
+        System.out.print(inches);
+    }
+
+
+}
