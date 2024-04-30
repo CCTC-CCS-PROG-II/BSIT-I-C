4568452f05f53cc7dc4feee52eb420f2d028dcab
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-04-30 13:59:21 +0800

    Add files via upload

---
+++ Activities/Activity I/FLORES_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+public class FLORES_ACTIVITY_I {
+        public static void main(String []args){
+    
+           int a = 20;
+           int b = 30;
+           int c = 65;
+    
+           System.out.println(a - b);
+           System.out.println(a + c);
+           System.out.println(b / a);
+           System.out.println(a * c);
+    
+    
+        }
+    }
+    
+}
