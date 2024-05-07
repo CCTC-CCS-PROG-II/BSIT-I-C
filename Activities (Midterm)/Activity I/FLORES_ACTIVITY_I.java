a81b8387d258adfccc4f1a37aa7147b38f54ef55
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-05-07 10:36:17 +0800

    Update and rename FLORES_ACTIVITY_1.java to FLORES_ACTIVITY_I.java

--- Activities (Midterm)/Activity I/FLORES_ACTIVITY_I.java
+++ Activities (Midterm)/Activity I/FLORES_ACTIVITY_I.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FLORES_ACTIVITY_1 {
+public class FLORES_ACTIVITY_I {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
