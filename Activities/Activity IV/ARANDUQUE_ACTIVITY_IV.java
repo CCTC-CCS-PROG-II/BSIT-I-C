026bdc2d1b94d3f341a96b84494433d03e673cd4
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-05-07 10:53:01 +0800

    Update ARANDUQUE_ACTIVITY_IV.java

--- Activities/Activity IV/ARANDUQUE_ACTIVITY_IV.java
+++ Activities/Activity IV/ARANDUQUE_ACTIVITY_IV.java
@@ -1,10 +1,10 @@
 import.java.util.Scanner;
 public class ARANDUQUE_ACTIVITY_IV{
-
-   public static void main(String{} args){
+  public static void main(String{} args){
+     
       Scanner sc = new Scanner(System.in);
-
-      System.out.println(" Enter the colr of the item: ");
+      System.out.print(" Enter the colr of the item: ");
+     
       String color = sc.nextline();
 
 switch (color) {
