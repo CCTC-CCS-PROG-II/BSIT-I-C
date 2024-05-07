eae9dc8c877533d821f9554032a9adbe0ed8fc09
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-05-07 11:16:15 +0800

    Update and rename ARANDUQUE_ACTIVITY_I.java to ARANDUQUE_ACTIVITY_I.java

---
+++ Activities/Activity I/ARANDUQUE_ACTIVITY_I.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ARANDUQUE_ACTIVITY_I {
+    public static void main(String[] args){
+        int x = 30;
+        int y = 25;
+        int f = 40;
+        int z = x+y;
+        int s = x-f;
+        int c = f*x;
+    
+
+        System.out.println("Addition = " + z);
+        System.out.println("Difference = " + s);
+        System.out.println("Product:" + c);
+    }
+
+}  
