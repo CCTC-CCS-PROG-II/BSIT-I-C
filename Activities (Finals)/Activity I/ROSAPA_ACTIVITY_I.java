e87b76ef30f4496f99b34fe0aaf8ce04955aee09
Johniño R. Rosapa <156732181+Johnino123@users.noreply.github.com>
2024-05-07 10:38:27 +0800

    Update ROSAPA_ACTIVITY_I.java

--- Activities (Finals)/Activity I/ROSAPA_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/ROSAPA_ACTIVITY_I.java
@@ -10,15 +10,11 @@ public class ROSAPA_ACTIVITY_I {
     static int getLength() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the length: ");
-        int length = sc.nextInt();
-        sc.close();
-        return length;
+        return sc.nextInt();
     }
     static int getWidth() {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the width: ");
-        int width = sc.nextInt();
-        sc.close();
-        return width;
+        return sc.nextInt();
     }
 }
