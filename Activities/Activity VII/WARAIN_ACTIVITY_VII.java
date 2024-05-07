00ff2042b01cca607d57bfe27a448a308475ef37
Clarence Nino Warain <156734315+N1N25@users.noreply.github.com>
2024-05-07 10:46:27 +0800

    Update and rename Warain_Activity_VII.java to WARAIN_ACTIVITY_VII.java

--- Activities/Activity VII/WARAIN_ACTIVITY_VII.java
+++ Activities/Activity VII/WARAIN_ACTIVITY_VII.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Warain_Activity_VII {
+public class WARAIN_ACTIVITY_VII {
 
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
