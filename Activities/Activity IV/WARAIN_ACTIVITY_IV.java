8672434c2d85b2647b6678abf918555c8578d3a1
Clarence Nino Warain <156734315+N1N25@users.noreply.github.com>
2024-05-07 10:38:41 +0800

    Update WARAIN_ACTIVITY_IV.java

--- Activities/Activity IV/WARAIN_ACTIVITY_IV.java
+++ Activities/Activity IV/WARAIN_ACTIVITY_IV.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
-    public class WARAIN_ACTIVITY_IV{
-        public static void main(String[] args){
 
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a color: ");
-            
-            String color = sc.nextline();
+public class WARAIN_ACTIVITY_IV {
 
-            switch (color){
-                case "red":
-                    System.out.println(1);
-                    break;
-                case "green":
-                    System.out.println(2);
-                    break;
-                case "black":
-                    System.out.println(3);
-                    break;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            } 
+        String color;
+
+        System.out.println("Enter your color you want to display (Red, Green, Black)");
+        color = scanner.nextLine();
+
+        switch (color) {
+            case "Red":
+                System.out.println("1");
+                
+                break;
+            case "Green":
+                System.out.println("2");
+                
+                break;
+            case "Black":
+                System.out.println("3");
+                
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter Red, Green, or Black.");
+                break;
         }
 
+       
     }
+}
