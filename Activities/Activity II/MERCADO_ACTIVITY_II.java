5d5dade5d6d6dfd449d2cf587fc600fc1b7a5b8f
Lianne Gabrielle U. Mercado <156732481+liannemercado@users.noreply.github.com>
2024-05-07 11:09:07 +0800

    Update and rename Mercado_Activity_II.java to MERCADO_ACTIVITY_II.java

--- Activities/Activity II/MERCADO_ACTIVITY_II.java
+++ Activities/Activity II/MERCADO_ACTIVITY_II.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Mercado_Activity_II {
+public class MERCADO_ACTIVITY_II {
     public static void main(String[] args) {
        Scanner sc = new Scanner (System.in);
 
