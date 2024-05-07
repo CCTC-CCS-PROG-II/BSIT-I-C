d6a1128315fb1625bdbf1940a10d9521b5e946cf
James Karl Heyres <157438154+Jameskarlheyres@users.noreply.github.com>
2024-05-07 10:40:58 +0800

    Add files via upload

---
+++ Activities (Midterm)/Activity III/HEYRES_ACTIVITY_III.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+  public class HEYRES_ACTIVITY_III {
+      public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+          
+          int scores [][] = new int [5][5];
+          int totalscore = 0;
+          double average;
+
+          System.out.println("Input your Scores: ");
+            
+
+           for(int R = 0; R<scores.length; R++ ) {
+              for(int C = 0; C<scores[R].length; C++) {
+              scores[R][C] = sc.nextInt();
+              totalscore += scores[R][C];
+
+              }
+           }
+
+           average = (double) totalscore / (scores.length * scores[0].length);
+
+          System.out.println("Average: "+average);
+
+
+      }
+  } 
