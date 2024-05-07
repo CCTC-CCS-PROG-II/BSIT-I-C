641da108c00a271510e729fac66ac7f13c689855
Cathrine May A. Cañoneo <156732189+CathrineCanoneo@users.noreply.github.com>
2024-05-07 11:04:11 +0800

    Update and rename Canoneo_Activity_IV.java to CANONEO_ACTIVITY_IV.java

--- Activities/Activity IV/CANONEO_ACTIVITY_IV.java
+++ Activities/Activity IV/CANONEO_ACTIVITY_IV.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Canoneo_Activity_IV {
+public class CANONEO_ACTIVITY_IV {
     public static void main(String[] args){
         Scanner sc = new Scanner ( System.in);
 
