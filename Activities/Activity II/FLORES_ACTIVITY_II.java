92a966728407fec5c2896a4b9a86aa4963473504
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-05-07 10:31:52 +0800

    Update and rename flores_Activity_2.java to FLORES_ACTIVITY_II.java

--- Activities/Activity II/FLORES_ACTIVITY_II.java
+++ Activities/Activity II/FLORES_ACTIVITY_II.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Flores_Activity_2 {
+public class FLORES_ACTIVITY_II {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first number: ");
