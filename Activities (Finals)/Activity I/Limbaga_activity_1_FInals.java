7686a119f3fc79acbf72339a3bf695f4883e20d5
Ken M Limbaga <156732339+kenlimbaga@users.noreply.github.com>
2024-05-07 10:28:01 +0800

    Update Limbaga_activity_1_FInals.java

---
+++ Activities (Finals)/Activity I/Limbaga_activity_1_FInals.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Limbaga_activity_1_FInals {
+    static void main(String[] args){
+       int length = getlength();
+       int widt =  getwidth();
+       int area = lenght * width();
+       
+       static int getLength(){
+        Scanner sc=  new Scanner(System.in);
+        System.out.println("Enter the value of the length: ");
+        return= sc.nextInt();
+    }
+
+    static int getWidth(){ 
+        Scanner sc=  new Scanner(System.in);
+        System.out.println("Enter the value of the width: ");
+        return= sc.nextInt()
+    }
+}
+
+}
