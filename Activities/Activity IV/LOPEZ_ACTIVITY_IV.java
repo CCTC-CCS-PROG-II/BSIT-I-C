67dc33c54b6592c64b1f4b47bbfd188fb1449ea8
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-05-07 10:47:53 +0800

    Update and rename Lopez_Activity_4.java to LOPEZ_ACTIVITY_IV.java

---
+++ Activities/Activity IV/LOPEZ_ACTIVITY_IV.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class LOPEZ_ACTIVITY_IV{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        String color;
+        System.out.print("Enter the color: ");
+        color = input.nextLine();
+
+        switch(color){
+            case "purple":
+                System.out.println("8");
+                break;
+            case "pink":
+                System.out.println("5");
+                break;
+            case "black":
+                System.out.println("18");
+                break;
+        }
+    }
+}
+
+
+
+
+
+
+
+
