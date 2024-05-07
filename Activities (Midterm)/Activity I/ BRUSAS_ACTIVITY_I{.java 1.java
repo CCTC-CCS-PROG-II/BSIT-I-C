7b215a0264ed90b4e1becaeb4605303ac139167e
Andrew B. Brusas <156732631+drewbubuli@users.noreply.github.com>
2024-05-06 20:20:21 -0700

    Update and rename class Andrew_Activity_I{.java 1.java to  BRUSAS_ACTIVITY_I{.java 1.java

--- Activities (Midterm)/Activity I/ BRUSAS_ACTIVITY_I{.java 1.java
+++ Activities (Midterm)/Activity I/ BRUSAS_ACTIVITY_I{.java 1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-   public class Brusas_ACTIVITY_I {
+   public class BRUSAS_ACTIVITY_I {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
