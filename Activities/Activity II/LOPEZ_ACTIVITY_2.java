2d36158ae57ecff20c8630de5dbdf87922f4dca4
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-05-07 10:42:22 +0800

    Update and rename Lopez_activity_2.java to LOPEZ_ACTIVITY_2.java

---
+++ Activities/Activity II/LOPEZ_ACTIVITY_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class LOPEZ_ACTIVITY_II {
+public static void main (String[] args){
+ Scanner input= new Scanner (System.in);
+ System.out.print("Enter first number: ");
+ int fnum= input.nextInt();
+ System.out.print("Enter first number: ");
+ int snum= input.nextInt();
+
+ int subtract = fnum-snum;
+ int add = fnum+snum;
+ int product = fnum*snum;
+
+ System.out.println("Difference = " + subtract); 
+ System.out.println("Sum = " + add);
+ System.out.println("Product = " + product);
+
+
+}
+}
+
+
+
+
+
+
