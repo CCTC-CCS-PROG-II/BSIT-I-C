88d9e40a3c33e14b0b88c9f46f07b50cef089bf3
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-05-07 10:48:51 +0800

    Update Aranduque_activity_1.java

--- Activities/Activity I/Aranduque_activity_1.java
+++ Activities/Activity I/Aranduque_activity_1.java
@@ -1,18 +1,17 @@
 public class Aranduque_activity_1 {
     public static void main(String[] args) {
-        int a = 30;
-        int b = 25;
-        int c = 40;
+        int x = 30;
+        int y = 25;
+        int f = 40;
 
-        int d = a+b;
-        int e = a+c;
-        int f = c*a;
-        int g = a/b;
+        int z = x+y;
+        int s = x-f;
+        int c = f*x;
+    
 
-        System.out.println("Addition:" + d);
-        System.out.println("Multiplication:" + e);
-        System.out.println("Subtraction:" + f);
-        System.out.println("Division:" + g);
+        System.out.println("Addition = " + z);
+        System.out.println("Difference = " + s);
+        System.out.println("Product:" + c);
     }
 
 }
