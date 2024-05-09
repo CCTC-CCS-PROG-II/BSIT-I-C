ff7b861ad3fea01228e259343c35acd4780fa48b
JamesCanoy <158119952+JamesCanoy@users.noreply.github.com>
2024-05-09 14:18:40 +0800

    Add files via upload

---
+++ Activities/Activity I/CANOY_ACTIVITY_I.java
@@ -0,0 +1,20 @@
+public class CANOY_ACTIVITY_I {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 20;
+        int f = 30;
+        int z = x+y;
+        int s = x-f;
+        int c = f*x;
+
+        
+    
+        System.out.println("Sum = " + z);
+        System.out.println("Difference = " + s);
+        System.out.println("Product = " + c);
+    
+        
+     
+    }
+
+}
