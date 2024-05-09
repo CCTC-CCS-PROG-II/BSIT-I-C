badc995d7c4b8ff0c1514c84395ee7eee4078ac9
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-05-09 16:54:12 +0800

    Update ARANDUQUE_ACTIVITY_VII.java

--- Activities/Activity VII/ARANDUQUE_ACTIVITY_VII.java
+++ Activities/Activity VII/ARANDUQUE_ACTIVITY_VII.java
@@ -1,17 +1,17 @@
-import java.util.Scanner{}
+import java.util.Scanner;
 public class ARANDUQUE_ACTIVITY_VII {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("Enter factorial Number:");
-
-        int num =sc.nextInt();
+        System.out.print("Enter a non-negative integer: ");
+        long num = sc.nextInt();
+        long factorial=1;
         
-        long factional = 1;
-        for(int i = 1; i <= num; i++) {
-           factorial *=1;
+        
+        for(int i=1; i<= num; i++ ){
+           factorial*=1;
         } 
-           System.out.println(" The factorial of " num + " is 5" + factorial);
+           System.out.println(" The factorial of "+num + " is: "+factorial);
 
         }
     }
