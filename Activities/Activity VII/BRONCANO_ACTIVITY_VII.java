b067e9a72315b20961bfcf6d057f4b1e60ad3b6e
Jenishe A. Broncano <156733649+JenisheBroncano@users.noreply.github.com>
2024-05-09 16:45:24 +0800

    Update BRONCANO_ACTIVITY_VII.java

--- Activities/Activity VII/BRONCANO_ACTIVITY_VII.java
+++ Activities/Activity VII/BRONCANO_ACTIVITY_VII.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-
 public class BRONCANO_ACTIVITY_VII {
     public static void main(String []args) {
         Scanner sc= new Scanner (System.in);
 
-        System.out.print("Enter Factional Number:");
-        long number =sc.nextInt()
-        fact = 1;
+        System.out.print("Enter Factional Number: ");
+        long number =sc.nextInt();
+        long fact = 1;
         for (long x=1;x<=number;x++){
             fact*=x;
         }
