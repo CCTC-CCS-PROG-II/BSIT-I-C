2e43b35f715a1701cf4a542e06bab6908cb92300
Kathlien grace Arnido <156732552+kathlien@users.noreply.github.com>
2024-05-09 15:58:13 +0800

    Update ARNIDO_ACTIVITY_ll.java

--- Activities (Finals)/Activity II/ARNIDO_ACTIVITY_ll.java
+++ Activities (Finals)/Activity II/ARNIDO_ACTIVITY_ll.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ARNIDO_ACTIVITY_2{
+public class ARNIDO_ACTIVITY_II{
     public static void main (String[]agrs) {
         double res = feet * 12;
         System.out.println(res);
