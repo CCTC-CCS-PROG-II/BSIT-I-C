7128a91823b57fa81b0034581a8e10feaf93005a
Ken M Limbaga <156732339+kenlimbaga@users.noreply.github.com>
2024-05-09 11:31:37 +0800

    Add files via upload

---
+++ Activities/Activity I/LIMBAGA_ACTIVITY_I.java
@@ -0,0 +1,18 @@
+public class LIMBAGA_ACTIVITY_I{
+    public static void main(String[] args){
+   
+     int a = 25;
+     int b = 45;
+     int c = 56;
+     int d = 86;
+     
+
+    int X = c * b;
+    int Z = d + c;
+    System.out.println(d);
+    System.out.println(c);
+
+   }  
+
+
+}
