f5d8836439b8b05b4cecdc3c84559ad6a04f20e9
JohnLouisPadayao <157442631+JohnLouisPadayao@users.noreply.github.com>
2024-05-09 02:24:35 -0400

    Add files via upload

---
+++ Activities/Activity II/PADAYAO_ACTIVITY_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+  class PADAYAO_ACTIVITY_II {
+  public static void main (String []args) {
+    Scanner sc = new Scanner(System.in);
+    int number1;
+    int number2;
+    
+   
+    System.out.println("Please put a Number: ");
+    number1 = sc.nextInt();
+
+    System.out.println("Please put a Number: ");
+    number2 = sc.nextInt();
+    
+    int A = number1 + number2;
+    int B = number1 - number2;
+    int C = number1 * number2;
+
+    System.out.println("Sum = "+A); //Addition
+    System.out.println("Difference = "+B); //Subtraction
+    System.out.println("Product = "+C); //Multiplication 
+
+    sc.close();
+   
+  }
+}
