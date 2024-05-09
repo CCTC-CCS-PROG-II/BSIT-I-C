fd2b032ef9dafbf6e9a36c243555d9e087b3f7bf
Claire Labadlabad <156732347+ClaireLabadlabad@users.noreply.github.com>
2024-05-09 16:11:19 +0800

    Update LABADLABAD_ACTIVITY_II.java

--- Activities/Activity II/LABADLABAD_ACTIVITY_II.java
+++ Activities/Activity II/LABADLABAD_ACTIVITY_II.java
@@ -4,7 +4,7 @@ public static void main (String[] args){
  Scanner input= new Scanner (System.in);
  System.out.print("Enter first number: ");
  int fnum= input.nextInt();
- System.out.print("Enter first number: ");
+ System.out.print("Enter second number: ");
  int snum= input.nextInt();
 
  int subtract = fnum-snum;
