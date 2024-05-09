ce73bde12377753eb86aac0d4efdbed8baeae201
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-05-09 16:35:41 +0800

    LASTIMOSA_ACTIVITY_VI.java

--- Activities/Activity VI/LASTIMOSA_ACTIVITY_VI.java
+++ Activities/Activity VI/LASTIMOSA_ACTIVITY_VI.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class LASTIMOSA_ACTIVITY_VI{
-    public static void main (String[]args){
 
-    Scanner sc= new Scanner(System.in);
-    int num;
-    System.out.print("Enter the no. of seconds: ");
-    int count = sc.nextInt();
-    
-    while(count>=100){
-        System.out.println(" Countdown");
-        x--;
+public class LASTIMOSA_ACTIVITY_VI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of seconds: ");
+        int count = sc.nextInt();
+        
+        while (count >= 0) {
+            System.out.println("Countdown: " + count);
+            count--;
+        }
+        
+        System.out.println("Countdown complete!");
     }
-    }
-    }
+}
