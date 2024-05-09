b41fbc2cd57cf4f00aae3a9414fc481e8477e1eb
Hans Rafael U. Villanueva <156732376+loki123456789890@users.noreply.github.com>
2024-05-09 11:29:01 +0800

    Update VILLANUEVA_ACTIVITY_II.java

--- Activities/Activity II/VILLANUEVA_ACTIVITY_II.java
+++ Activities/Activity II/VILLANUEVA_ACTIVITY_II.java
@@ -4,9 +4,9 @@ public class VILLANUEVA_ACTIVITY_II {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the first number");
+        System.out.println("Enter the first number: ");
         int a = sc.nextInt();
-        System.out.println("Enter the second number");
+        System.out.println("Enter the second number: ");
         int b = sc.nextInt();
 
         int sum = a + b;
