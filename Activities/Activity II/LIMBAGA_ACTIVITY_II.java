3846d232222d9152ba17e07bbf0b5c3ee2a61260
Ken M Limbaga <156732339+kenlimbaga@users.noreply.github.com>
2024-05-09 11:37:24 +0800

    Add files via upload

---
+++ Activities/Activity II/LIMBAGA_ACTIVITY_II.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class LIMBAGA_ACTIVITY_II {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Second Number: ");
+        int num2 = sc.nextInt();
+
+        int ans1 = num1+num2;
+        int ans2 = num1-num2;
+        int ans3 = num1*num2;
+
+        System.out.println("Sum = "+ans1);
+        System.out.println("Difference = "+ans2);
+        System.out.println("Product = "+ans3);
+    }
+}
