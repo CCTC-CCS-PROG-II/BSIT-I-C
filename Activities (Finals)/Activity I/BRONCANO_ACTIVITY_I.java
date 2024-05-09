137279310079f2147f6c68ef8e8bf8e64792300b
Jenishe A. Broncano <156733649+JenisheBroncano@users.noreply.github.com>
2024-05-09 16:51:50 +0800

    Update BRONCANO_ACTIVITY_I.java

--- Activities (Finals)/Activity I/BRONCANO_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/BRONCANO_ACTIVITY_I.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
-    public class BROBCANO_ACTIVITY_I {
-        public static void main(String args[]) 
-        {   
+public class BROBCANO_ACTIVITY_I {
+    public static void main(String args[]) 
+    {   
 
-            int length = getlength();
-            int width = getWidth() ;
-            int area = length*width;
-             System.out.println("Area - " + area);    
-       }
-       static int getlength(){
-          Scanner input = new Scanner (System.in);
-          System.out.println("Input your Length");
-          return input.nextInt();
-       }
-       static int getWidth(){
-          Scanner input = new Scanner (System.in);
-          System.out.println("Input your Width");
-          return input.nextInt();
+        int length = getlength();
+        int width = getWidth() ;
+        int area = length*width;
+         System.out.println("Area - " + area);    
+    }
+    static int getlength(){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input your Length");
+        return input.nextInt();
+    }
+    static int getWidth(){
+        Scanner input = new Scanner (System.in);
+        System.out.println("Input your Width");
+        return input.nextInt();
     }
     }
-     
-
-}
