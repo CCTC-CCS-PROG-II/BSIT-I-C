0bd6df28454e85ea70c1ef840d7e611ddfba9df7
Flores620 <158122628+Flores620@users.noreply.github.com>
2024-05-09 12:49:58 +0800

    Update FLORES_ACTIVITY_VII.java

--- Activities/Activity VII/FLORES_ACTIVITY_VII.java
+++ Activities/Activity VII/FLORES_ACTIVITY_VII.java
@@ -1,18 +1,16 @@
-
 import java.util.Scanner;
+
 public class FLORES_ACTIVITY_VII {
-
     public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
-
-        System.out.println("Enter a non-negative number:");
-        int num = sc.nextInt();
-
-        long factorial = 1;
-        for (int i = 1; i<= num; i++) {
-            factorial *= 1;
-        }
-        System.out.println("The Factorial of "+ num + "factorial");
+        Scanner sc= new Scanner (System.in);
         
+        System.out.print("Enter Your Factorial Number ");
+        long number =sc.nextInt();
+        long fact =1;
+        for(long x=1;x<=number;x++){
+            fact*=x;
+        }
+        System.out.print(fact);
+        sc.close();
     }
 }
