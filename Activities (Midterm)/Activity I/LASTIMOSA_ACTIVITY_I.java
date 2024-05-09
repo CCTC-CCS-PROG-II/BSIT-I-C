1c3757333c0dd77e2a26257d775b2709ca238d5a
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-05-09 09:58:19 +0800

    Update LASTIMOSA_ACTIVITY_I.java

--- Activities (Midterm)/Activity I/LASTIMOSA_ACTIVITY_I.java
+++ Activities (Midterm)/Activity I/LASTIMOSA_ACTIVITY_I.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
 
-       String [] names = new String[5];""
+       String [] names = new String[5];
 
        for (int i = 0; i < names.length; i++) {
         System.out.print("Enter name" + (i+1) + ":");
