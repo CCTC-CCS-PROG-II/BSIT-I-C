f98f2eaf8ecbfd41f7511c3a70bf468acd8ca082
JohnLouisPadayao <157442631+JohnLouisPadayao@users.noreply.github.com>
2024-05-09 02:16:16 -0400

    Add files via upload

---
+++ Activities/Activity I/PADAYAO_ACTIVITY_1.java
@@ -0,0 +1,18 @@
+public class padayao_ACTIVITY_1 {
+  public static void main (String []args) {
+
+      int Q = 10;
+      int W = 20;
+      int E = 30;
+
+      int A = Q + W;
+      int B = W - Q;
+      int C = Q * E;
+      int D = E / Q;
+
+      System.out.println("Addition = "+A); //Addition
+      System.out.println("Substraction = "+B); //Subtraction
+      System.out.println("Multiplication = "+C); //Multiplication
+      System.out.println("Division = "+D); //Division
+  }
+}
