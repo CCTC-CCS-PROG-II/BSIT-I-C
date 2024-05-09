0a496790a9bde44a1b31151393264f1f29e18ca5
Michelle Grace S. Lugatiman <156732421+lugatiman@users.noreply.github.com>
2024-05-09 16:50:10 +0800

    Update LUGATIMAN_ACTIVITY_I.java

--- Activities (Finals)/Activity I/LUGATIMAN_ACTIVITY_I.java
+++ Activities (Finals)/Activity I/LUGATIMAN_ACTIVITY_I.java
@@ -1,23 +1,25 @@
 
 import java.util.Scanner;
 public class LUGATIMAN_ACTIVITY_I {
-    public static void main(String[]args){
-     int length= getLength();
-     int width= getWidth();
-     int area= length*width;
-     system.out.println("The area of the rectangle is: " +area);
+    public static void main(String args[]) 
+    {   
 
-        static int getLength(){
-            Scanner sc=  new Scanner(System.in);
-            System.out.println("Enter the value of the length: ");
-            return= sc.nextInt();
-        }
-
-        static int getWidth(){ 
-            Scanner sc=  new Scanner(System.in);
-            System.out.println("Enter the value of the width: ");
-            return= sc.nextInt()
-        }
-    }
-    
+        int length = getlength();
+        int width = getWidth() ;
+        int area = length*width;
+         System.out.println("Area - " + area);    
+   }
+   static int getlength(){
+      Scanner input = new Scanner (System.in);
+      System.out.println("Input your Length:");
+      return input.nextInt();
+   }
+   static int getWidth(){
+      Scanner input = new Scanner (System.in);
+      System.out.println("Input your Width:");
+      return input.nextInt();
 }
+}
+
+    
+
