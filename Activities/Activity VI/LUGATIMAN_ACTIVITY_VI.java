394772b36a340568cd9b324b177b0b37c2105094
Michelle Grace S. Lugatiman <156732421+lugatiman@users.noreply.github.com>
2024-05-09 16:06:20 +0800

    Update LUGATIMAN_ACTIVITY_VI.java

--- Activities/Activity VI/LUGATIMAN_ACTIVITY_VI.java
+++ Activities/Activity VI/LUGATIMAN_ACTIVITY_VI.java
@@ -1,27 +1,16 @@
 import java.util.Scanner;
 public class LUGATIMAN_ACTIVITY_VI {
     public static void main (String []args){
-
      Scanner sc= new Scanner(System.in);
 
-    System.out.print("Enter the number of Seconds: ");
-    int sec= sc.nextInt();
-
+    System.out.print("Enter the number: ");
+    int seconds= sc.nextInt();
      
-   
-   
-     
-while
-        (count>=0){
-            system.out.println("second:");
-            
-        sec--;
+while(seconds >= 0){
+            System.out.println(seconds);    
+        seconds--;
      }
-
-
-
      }
 
 }
     
-
