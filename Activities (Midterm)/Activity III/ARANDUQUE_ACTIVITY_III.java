065b751461f51127625f057f74448890b0202634
Gea M. Aranduque <156732461+GeaAranduque@users.noreply.github.com>
2024-05-09 16:59:58 +0800

    Update ARANDUQUE_ACTIVITY_III.java

--- Activities (Midterm)/Activity III/ARANDUQUE_ACTIVITY_III.java
+++ Activities (Midterm)/Activity III/ARANDUQUE_ACTIVITY_III.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 public class ARANDUQUE_ACTIVITY_III {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+   public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
 
-        int[][] scores = new int[5][5];
+        int [][] score = new int [5][5];
 
-        for (int x = 0; i < scores.length; x++) {
-            System.out.println("Enter Scores " + (x + 1) + ":");
-            for (int y = 0; y < scores[i].length; y++) {
-                scores[x][y] = input.nextInt();
+        for (int x = 0; x < score.length; x++){
+            for (int y = 0; y < score.length; y++){
+                System.out.print("Enter score for Row " + (x + 1) + ", Column " + (y+1) + ":");
+                score[x][y] = sc.nextInt();
             }
+
         }
 
         int sum = 0;
         int count = 0;
-        for (int x = 0; x < scores.length; x++) {
-            for (int y = 0; y < scores[x].length; y++) {
-                sum += scores[x][y];
+        for (int x = 0; x < score.length; x++) {
+            for (int y = 0; y < score.length; y++) {
+                sum += score [x][y];
                 count++;
             }
         }
 
-        double average = sum / count;
-        System.out.println("Total Score Average: " + average);
+        double average = (double) sum / count;
+        System.out.println("The Average of the Scores: " + average);
     }
-}
+ }
