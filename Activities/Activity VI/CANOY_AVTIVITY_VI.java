1e995869db60982d7558dd6bf3add3dc5c2d1647
JamesCanoy <158119952+JamesCanoy@users.noreply.github.com>
2024-05-09 14:20:09 +0800

    Update CANOY_AVTIVITY_VI.java

--- Activities/Activity VI/CANOY_AVTIVITY_VI.java
+++ Activities/Activity VI/CANOY_AVTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ALEDO_ACTIVITY_VI {
+public class CANOY_ACTIVITY_VI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of seconds: ");
