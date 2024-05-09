975bff73ceda87d32e509755b9aad762ca66acce
Jeniña Cabisas <156732411+ninyangg@users.noreply.github.com>
2024-05-09 17:01:55 +0800

    Update CABISAS_ACTIVITY_VII.java

--- Activities/Activity VII/CABISAS_ACTIVITY_VII.java
+++ Activities/Activity VII/CABISAS_ACTIVITY_VII.java
@@ -5,11 +5,11 @@ public class CABISAS_ACTIVITY_VII {
 
         System.out.print("Please enter a non-negative integer: ");
 
-        int num = sc.nextInt();
+        int num = input.nextInt();
 
         long factorial = 1;
-        for(int i = 1; i <= num; i++){
-           factorial *= i;
+        for (int i = 1; i <= num; i++){
+            factorial *= i;
         }
            System.out.println("The factorial of " + num + " is 5" + factorial);      
     }
