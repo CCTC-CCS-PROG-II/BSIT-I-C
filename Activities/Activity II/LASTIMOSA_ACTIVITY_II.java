542486efa9ce226351e3554eb2097f6361c3272a
Christian Lastimosa <156734087+christianskie123@users.noreply.github.com>
2024-05-09 16:33:15 +0800

    Update LASTIMOSA_ACTIVITY_II.java

--- Activities/Activity II/LASTIMOSA_ACTIVITY_II.java
+++ Activities/Activity II/LASTIMOSA_ACTIVITY_II.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
+
 public class LASTIMOSA_ACTIVITY_II {
-public static void main (String[] args){
- Scanner input= new Scanner (System.in);
- System.out.print("Enter first number: ");
- int fnum= input.nextInt();
- System.out.print("Enter first number: ");
- int snum= input.nextInt();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int fnum = input.nextInt();
+        System.out.print("Enter second number: "); // Fixed typo here
+        int snum = input.nextInt();
 
- int subtract = fnum-snum;
- int add = fnum+snum;
- int product = fnum*snum;
+        int subtract = fnum - snum;
+        int add = fnum + snum;
+        int product = fnum * snum;
 
- System.out.println("Difference = " + subtract); 
- System.out.println("Sum = " + add);
- System.out.println("Product = " + product);
-
-
+        System.out.println("Difference = " + subtract);
+        System.out.println("Sum = " + add);
+        System.out.println("Product = " + product);
+    }
 }
