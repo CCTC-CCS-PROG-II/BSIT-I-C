a3567ac8adc1e0f9826b8d83d953fc37b2263c5f
Carmel Anne Labandero <156732253+caramelanneee@users.noreply.github.com>
2024-05-09 15:58:10 +0800

    Update LABANDERO_ACTIVITY_VI.java

--- Activities/Activity VI/LABANDERO_ACTIVITY_VI.java
+++ Activities/Activity VI/LABANDERO_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class LABANDERO_ACTIVITY_XI {
+public class LABANDERO_ACTIVITY_VI {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
 
