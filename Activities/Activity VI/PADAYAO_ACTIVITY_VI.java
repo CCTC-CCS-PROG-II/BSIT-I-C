ddc3cdb711bed63f64711752142599264fa7780c
JohnLouisPadayao <157442631+JohnLouisPadayao@users.noreply.github.com>
2024-05-09 02:30:50 -0400

    Update and rename Padayao_ACTIVITY_VI.java to PADAYAO_ACTIVITY_VI.java

--- Activities/Activity VI/PADAYAO_ACTIVITY_VI.java
+++ Activities/Activity VI/PADAYAO_ACTIVITY_VI.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Padayao_ACTIVITY_VI {
+public class PADAYAO_ACTIVITY_VI {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
