d8a9f412c795c015856d2fc11a95f666bccfb0e0
Seras Reymann <157442457+reymannseras11@users.noreply.github.com>
2024-05-09 12:30:09 +0800

    Add files via upload

---
+++ Activities/Activity I/SERAS_ACTIVITY_I.java
@@ -0,0 +1,15 @@
+public class SERAS_ACTIVITY_I {
+    public static void main(String []args){
+
+       int e = 20;
+       int f = 30;
+       int g = 65;
+
+       System.out.println(e - f);
+       System.out.println(e + g);
+       System.out.println(f / e);
+       System.out.println(e * g);
+
+
+    }
+}
