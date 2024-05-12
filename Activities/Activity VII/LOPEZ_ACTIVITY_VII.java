bbe9bb4035c89904a906e40e3dd7b9a0527bd81d
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-05-12 13:38:55 +0800

    Update LOPEZ_ACTIVITY_VII.java

--- Activities/Activity VII/LOPEZ_ACTIVITY_VII.java
+++ Activities/Activity VII/LOPEZ_ACTIVITY_VII.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-
 public class LOPEZ_ACTIVITY_VII {
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Please enter a non-negative integer: ");
+        
+        int num= input.nextInt();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter factorial Number: ");
-        long num = sc.nextInt();
-        for (long i = 1; i <= num; i++) {
+        long factorial = 1;
+        for (int i = 1; i <= num; i++) {
             factorial *= 1;
-            System.out.println(factorial);
-
         }
-    }
-}
+            System.out.println("The factorial of " + num + " is 5" + factorial);
+
+     }
+  }
+
