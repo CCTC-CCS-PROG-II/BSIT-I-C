dbd762dce476090ba27dae5fadcc07ddd8e4e42a
Mary Joyce P. Lopez <156732578+aestreadarwin@users.noreply.github.com>
2024-05-12 12:21:06 +0800

    Update LOPEZ_ACTIVITY_II.java

--- Activities/Activity II/LOPEZ_ACTIVITY_II.java
+++ Activities/Activity II/LOPEZ_ACTIVITY_II.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
 public class LOPEZ_ACTIVITY_II {
-public static void main (String[] args){
- Scanner input= new Scanner (System.in);
- System.out.print("Enter first number: ");
- int fnum= input.nextInt();
- System.out.print("Enter first number: ");
- int snum= input.nextInt();
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = input.nextInt();
+        System.out.print("Enter second number: ");
+        int b = input.nextInt();
 
- int subtract = fnum-snum;
- int add = fnum+snum;
- int product = fnum*snum;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
 
- System.out.println("Difference = " + subtract); 
- System.out.println("Sum = " + add);
- System.out.println("Product = " + product);
-
-
-}
+        System.out.println("Sum = "+sum); 
+        System.out.println("Difference = "+difference);
+        System.out.println("Product = "+product);
+     }
 }
 
 
